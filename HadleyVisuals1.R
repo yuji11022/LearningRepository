fcf4edfe1b6ba8b80c368931a03c18bdbeb8e59d
William Claster <william108@gmail.com>
2021-04-19 00:15:03 +0900

    adding in more examples from text

--- HadleyVisuals1.R
+++ HadleyVisuals1.R
@@ -13,7 +13,7 @@ ggplot(data = mpg) + geom_point(mapping = aes(x = class, y = drv))
 
 
 ggplot(data = mpg) + 
-  geom_point(mapping = aes(x = displ, y = hwy, stroke = class))
+  geom_point(mapping = aes(x = displ, y = hwy))
 View(mpg)
 
 # Left
@@ -23,3 +23,43 @@ ggplot(data = mpg) +
 # Right
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = displ, y = hwy),color="blue", shape=18)
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) + 
+  facet_wrap(~ class, nrow = 2)
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) + 
+  facet_grid(. ~ class)
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) + 
+  facet_grid(~ cty)
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) + 
+  facet_wrap(drv~ class, nrow = 2)
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_grid(drv ~ .)
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy)) +
+  facet_grid(. ~ cyl)
+
+ggplot(data = mpg) + 
+  geom_smooth(mapping = aes(x = displ, y = hwy, linetype=drv, color=drv))+geom_point(mapping =aes(x = displ, y = hwy, color=drv ), size=2)
+
+ggplot(data = mpg) +
+  geom_smooth(mapping = aes(x = displ, y = hwy, group = drv, color=drv))
+
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
+  geom_point(mapping = aes(color = class)) + 
+  geom_smooth(data = filter(mpg, class %in% c("subcompact","midsize")), se = FALSE, mapping=aes(color=class))
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
+  geom_point(mapping = aes(color= class)) + 
+  geom_smooth(data = filter(mpg, class %in%class ), se = FALSE, mapping=aes(color=class)) #not sure why I need class %in%class
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
+  geom_boxplot(mapping = aes(color = class)) + 
+  geom_smooth()
+
